c5040129ff6e2b104393fcb97122990b2ecab74d
RelativeDecay <ashton@eawalden.net>
2017-01-11 13:00:18 -0700

    Initial Commit

---
+++ Spoon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Spoon
+//
+//  Created by Ashton Walden on 1/11/17.
+//  Copyright © 2017 RelativeDecay. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
